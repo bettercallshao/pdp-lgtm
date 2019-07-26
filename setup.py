89fb3123a80bd62edaa3d348281b1ffefe1c58d2
Shaoqing Tan <shaoqing@stackpros.com>
2019-07-25 22:35:25 -0400

    Include README as long description

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
-from setuptools import setup, find_packages
-import sys, os
+from setuptools import find_packages, setup
 
 version = '0.1'
 
-setup(name='pdp-lgtm',
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='pdp-lgtm',
     version=version,
     description="party of dope pajamas looks good to me.",
+    long_description=long_description,
     author='Shaoqing Tan',
     author_email='tansq7@gmail.com',
     url='https://github.com/timlyrics/pdp-lgtm',
     license='MIT',
     packages=find_packages('src'),
-    package_dir = {'': 'src'},
+    package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
     scripts=['bin/pdp'],
-    entry_points = {
+    entry_points={
         'console_scripts': ['pdp.py=pdp_lgtm.pdp:main'],
     },
 )
